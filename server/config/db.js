bdadfa584f3c49e462835c2b80362ad7abfc7ed7
Lucas Fan <lucasfan1995@gmail.com>
2022-10-21 00:49:12 -0400

    update default value for edit page

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = 
 {
-    "URI" : "mongodb://localhost/business_contact"
-    //"URI" : "mongodb+srv://lucasfan:kunkun1122@mongodbserver.newnday.mongodb.net/book_store"
+    //"URI" : "mongodb://localhost/business_contact"
+    "URI" : "mongodb+srv://lucasfan:kunkun1122@mongodbserver.newnday.mongodb.net/business_contact"
 }
